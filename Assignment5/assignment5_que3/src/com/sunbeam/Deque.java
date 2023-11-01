9621065717d0f4f91aa0c295f3e8384489780385
Chetan-80097 <chetanarote5@gmail.com>
2023-11-01 17:16:01 +0530

    Assignment5 Que3  Uploaded

---
+++ Assignment5/assignment5_que3/src/com/sunbeam/Deque.java
@@ -0,0 +1,33 @@
+package com.sunbeam;
+
+public class Deque 
+{
+	private List l;
+	
+	public Deque()
+	{
+		l = new List();
+	}
+	
+	public void frontPush(int value)
+	{
+		l.addFirst(value);
+	}
+	
+	public void frontPop()
+	{
+		l.deleteLast();
+	}
+	
+	public void rearPop()
+	{
+		l.deleteFirst();
+	}
+	
+	public String peek()
+	{
+		int val = l.getData();
+		return "Peeked Element : "+val;
+	}
+
+}
---
+++ Assignment5/assignment5_que3/src/com/sunbeam/TestDequeMain.java
@@ -0,0 +1,18 @@
+package com.sunbeam;
+
+public class TestDequeMain 
+{
+
+	public static void main(String[] args)
+	{
+		Deque d = new Deque();
+		
+		d.frontPush(30);
+		d.frontPush(20);
+		d.frontPush(10);
+		System.out.println(d.peek());
+		d.rearPop();
+		System.out.println(d.peek());
+	}
+
+}
